67bf04951c51ca6869e3c3403a7cbfb54ba14fb3
isabelle khan <mahk-06@hotmail.com>
2023-04-13 15:33:37 +0200

    frotnend connection

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const app = require("express")();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
+app.use(express.static("/Users/isabellekhan/Datainteraction/TodolistUI"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
